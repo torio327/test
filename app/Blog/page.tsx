71a3f2ccc4dffb04cc18fbdae42bcc3a04ed8f8d
torio327 <128019081+torio327@users.noreply.github.com>
2024-01-25 11:45:54 +0900

    initial commit
    first

---
+++ app/Blog/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function page(){
+    return(
+        <>
+        <h1>Blog</h1>
+            <Link href={'/'}>Home</Link>
+        </>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+   <>
+   <h1>Home</h1>
+       <Link href={'/Blog'}>Blogへ</Link>
+   </>
+  );
+}
